bfed330e88a5599f9bb5bec9cc7942c75dc5ebef
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-23 16:36:14 -0500

    update about

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,15 +1,23 @@
 
 const AboutMe = () => {
     return (
-        <div className="bg-neutral-800">
-            <p className="text-black">hola</p>
-            <p className="text-black">hola</p>
-            <p className="text-black">hola</p>
-            <p className="text-black">hola</p>
-            <p className="text-black">hola</p>
-            <p className="text-black">hola</p>
-            <p className="text-black">hola</p>
+        <>
+        <div className="flex">
+            <div className="text-white px-8">
+                <h2>Eduardo</h2>
+                <h3>Software Developer</h3>  
+                <ul>
+                    <li> - Skills</li>
+                    <li> - Projects</li>
+                    <li> - About me</li>
+                    <li> - Contact me</li>
+                </ul>
+            </div>  
+            <div className="flex text-white">
+                <h2>Skills</h2>
+            </div>
         </div>
+        </>
     );
 };
 
