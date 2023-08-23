f9eddc229a28bf0458741340834b50255d740a42
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-23 11:59:15 -0500

    main completed

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,9 +1,22 @@
 
+import Button from './button';
+
 const main = () => {
+
+    
+
     return (
-        <div>
-            <h2>Eduardo Almachi</h2>
-            <h2>Software Developer</h2>
+        <div className='flex px-32 py-10 sm:px-52 sm:py-48'>
+
+            <div className='flex '>
+                <img src="/public/img/fotocarnet.jpg" className='my-5 hidden sm:block sm:h-auto sm:max-h-48 rounded-full'/>
+                <div className='p-5'>
+                    <h2 className='text-blue-950 font-bold text-lg'>Hi, I´m Eduardo Almachi 👋</h2>
+                    <h2 className='text-5xl font-extrabold py-2'>Software Developer</h2>
+                    <p className='py-1'>Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital word.</p>
+                    <Button/>
+                </div>
+            </div>
         </div>
     );
 };
