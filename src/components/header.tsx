2e6f5b9f7a5abf8dfeafb558ad1c5c618316d614
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-22 21:37:22 -0500

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {FaWhatsapp, FaLinkedin, FaGithub} from "react-icons/fa";
+import { AiOutlineMenu } from "react-icons/ai";
     
 
 const Header = () => {
@@ -11,40 +12,37 @@ const Header = () => {
     };
 
     return (
-            <nav className="flex items-center justify-between flex-wrap p-3">
+            <nav className="flex justify-between flex-wrap  shadow-lg p-3">
                 <div className="flex items-center flex-shrink-0 text- mr-6">
-                    <span className="font-semibold text-xl tracking-tight">EA</span>
+                    <span className="font-semibold tracking-tight pl-10 text-4xl">EA</span>
                 </div>
-                <div className="block lg:hidden">
-                    <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-black hover:text-white hover:border-white" onClick={toggleNav}>
-                        <svg className="fill-current h-3 w-3" ><title>Menu</title></svg>
+                <div className="block sm:hidden">
+                    <button className="flex items-center px-3 py-2 border rounded border-black hover:text-white hover:border-white" onClick={toggleNav}>
+                        <svg className="fill-current h-3 w-3" ><AiOutlineMenu></AiOutlineMenu></svg>
                     </button>
                 </div>
-                    <div className={`w-full flex-grow lg:flex lg:items-center lg:w-auto ${isNavOpen ? "block" : "hidden"}`}>
-                        <div className="text-sm lg:flex-grow">
-                            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black-300 hover:font-semibold mr-4">
-                                Home
-                            </a>
-                            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:font-semibold mr-4">
-                                Skills
-                            </a>
-                            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:font-semibold">
-                                Projects
-                            </a>
-
-                        </div>
-                    
-                        <a href="https://github.com/edusebass" target="blank" className="mx-1 inline-block text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 lg:mt-0 ">
-                            <FaGithub className=" text-4xl"/>                        
+                <div className={`w-full flex-grow justify-between sm:flex sm:items-center sm:w-auto ${isNavOpen ? "block" : "hidden"}`}>
+                    <div className="text-sm sm:flex-grow">
+                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-300 hover:font-semibold mr-4 uppercase sm:normal-case" >
+                            Home
                         </a>
-                        <a href="https://www.linkedin.com/in/eduardo-almachi-727730238/" target="blank" className={`mx-1 inline-block text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 lg:mt-0`}>
-                            <FaLinkedin className=" text-4xl"/>
+                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold mr-4 uppercase sm:normal-case">
+                            Skills
                         </a>
-                        <a href="https://api.whatsapp.com/send?phone=593986572316" target="blank" className={`mx-1 inline-block text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 lg:mt-0`}>
-                            <FaWhatsapp className=" text-4xl"/>
+                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold  uppercase sm:normal-case">
+                            Projects
                         </a>
-                    
-                    
+                    </div>
+                
+                    <a href="https://github.com/edusebass" target="blank" className="mx-1 hidden text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 sm:inline-block">
+                        <FaGithub className=" text-2xl"/>                        
+                    </a>
+                    <a href="https://www.linkedin.com/in/eduardo-almachi-727730238/" target="blank" className={`mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 hidden sm:inline-block`}>
+                        <FaLinkedin className=" text-2xl"/>
+                    </a>
+                    <a href="https://api.whatsapp.com/send?phone=593986572316" target="blank" className={`mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 hidden sm:inline-block`}>
+                        <FaWhatsapp className=" text-2xl"/>
+                    </a> 
                 </div>
             </nav>
         
