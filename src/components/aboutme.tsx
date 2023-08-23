95d78cc665f207209c16cdbd9a561e6d7eac2e5c
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-23 17:16:24 -0500

    update about

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,9 +1,15 @@
+import React, { useState, useEffect } from "react";
+import {BiLogoPython, BiLogoCPlusPlus, BiLogoHtml5, BiTerminal, BiLogoCss3, BiLogoJavascript, BiLogoNodejs, BiLogoReact, BiLogoJquery} from "react-icons/bi";
+import {FaJava} from "react-icons/fa";
+import {BsGit} from "react-icons/bs";
+import {SiJupyter, SiMysql, SiMicrosoftsqlserver, SiApachecouchdb} from "react-icons/si";
 
 const AboutMe = () => {
+    
     return (
         <>
         <div className="flex">
-            <div className="text-white px-8">
+            <div className="text-white px-8 bg-fixed " id="seccionestatica">
                 <h2>Eduardo</h2>
                 <h3>Software Developer</h3>  
                 <ul>
@@ -13,8 +19,70 @@ const AboutMe = () => {
                     <li> - Contact me</li>
                 </ul>
             </div>  
-            <div className="flex text-white">
+            <div className="text-white text-5xl" id="seccionquesibaja">
                 <h2>Skills</h2>
+                <div className="flex">
+                <BiLogoPython/>
+                <BiLogoCPlusPlus/>
+                <FaJava/>
+                <BiLogoHtml5/>
+                <BsGit/>
+                <BiTerminal/>
+                <BiLogoCss3/>
+                <BiLogoJavascript/> 
+                <SiJupyter/>
+                <SiMysql/>
+                <SiMicrosoftsqlserver/>
+                <SiApachecouchdb/>
+                <BiLogoNodejs/>
+                <BiLogoReact/>
+                <BiLogoJquery/>
+                
+                </div>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
+                <p>hola</p>
             </div>
         </div>
         </>
