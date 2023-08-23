e725c37455f797a86eef91891cf80f3715d425b5
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-23 12:12:42 -0500

    bug img fixed

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,6 @@
 
 import Button from './button';
+import imgPerfil from '../../public/img/fotocarnet.jpg';
 
 const main = () => {
 
@@ -9,7 +10,7 @@ const main = () => {
         <div className='flex px-32 py-10 sm:px-52 sm:py-48'>
 
             <div className='flex '>
-                <img src="/public/img/fotocarnet.jpg" className='my-5 hidden sm:block sm:h-auto sm:max-h-48 rounded-full'/>
+                <img src={imgPerfil} className='my-5 hidden sm:block sm:h-auto sm:max-h-48 rounded-full'/>
                 <div className='p-5'>
                     <h2 className='text-blue-950 font-bold text-lg'>Hi, I´m Eduardo Almachi 👋</h2>
                     <h2 className='text-5xl font-extrabold py-2'>Software Developer</h2>
