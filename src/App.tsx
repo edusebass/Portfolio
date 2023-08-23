0ad5cdeb3ab8135cfeb87cd22a85a2e508562bb5
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-22 23:46:23 -0500

    footer created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {} from 'react'
 import Header from './components/header';
 import Main from './components/main';
+import Footer from './components/footer';
 
 function App() {
   
@@ -9,7 +10,7 @@ function App() {
       <body className="font-mono min-h-screen bg-gradient-to-r from-white to-neutral-300">
         <Header />
         <Main />
-        
+        <Footer/>
       </body>
     </>
   )
