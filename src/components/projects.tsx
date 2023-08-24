e3eccff1584178606541844a6ad6d2d3dd336b65
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-23 20:27:39 -0500

    created proijects.tsx

---
+++ src/components/projects.tsx
@@ -0,0 +1,9 @@
+const Project = () => {
+    return (
+        <>
+            <h2 className="flex text-5xl font-bold text-yellow-500 justify-center">Projects</h2>
+        </>
+    )
+}
+
+export default Project;
