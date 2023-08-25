54c95e41e889a37f189b622cd687a533a8924be6
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-24 23:33:55 -0500

    modifeied

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
     };
 
     return (
-            <nav className="flex justify-between flex-wrap shadow-lg p-3 text-white">
+            <nav className=" flex justify-between flex-wrap shadow-lg p-3 text-white sm:fixed sm:p-4 sm:w-full">
                 <div className="flex items-center flex-shrink-0 text- mr-6">
                     <span className="font-semibold tracking-tight pl-10 text-4xl">EA</span>
                 </div>
@@ -23,30 +23,30 @@ const Header = () => {
                 </div>
                 <div className={`w-full flex-grow justify-between sm:flex sm:items-center sm:w-auto ${isNavOpen ? "block" : "hidden"}`}>
                     <div className="text-sm sm:flex-grow">
-                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-300 hover:font-semibold mr-14 uppercase sm:normal-case" >
+                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-300 hover:font-semibold hover:underline  mr-14 uppercase sm:normal-case" >
                             Home
                         </a>
-                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold mr-14 uppercase sm:normal-case">
+                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold hover:underline mr-14 uppercase sm:normal-case">
                             Skills
                         </a>
-                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold mr-14 uppercase sm:normal-case">
+                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold hover:underline mr-14 uppercase sm:normal-case">
                             Projects
                         </a>
-                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold  mr-14 uppercase sm:normal-case">
+                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold hover:underline mr-14 uppercase sm:normal-case">
                             About me
                         </a>
-                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold  mr-14 uppercase sm:normal-case">
+                        <a href="" className=" block mt-4 sm:inline-block sm:mt-0 text-black-200 hover:font-semibold hover:underline mr-14 uppercase sm:normal-case">
                             Contact
                         </a>
                     </div>
                 
-                    <a href="https://github.com/edusebass" target="blank" className="mx-1 hidden text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 sm:inline-block">
+                    <a href="https://github.com/edusebass" target="blank" className="mx-1 hidden text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 sm:inline-block sm:ease-in duration-300">
                         <FaGithub className=" text-2xl"/>                        
                     </a>
-                    <a href="https://www.linkedin.com/in/eduardo-almachi-727730238/" target="blank" className={`mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 hidden sm:inline-block`}>
+                    <a href="https://www.linkedin.com/in/eduardo-almachi-727730238/" target="blank" className={`mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 hidden sm:inline-block sm:sm:ease-in duration-300`}>
                         <FaLinkedin className=" text-2xl"/>
                     </a>
-                    <a href="https://api.whatsapp.com/send?phone=593986572316" target="blank" className={`mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 hidden sm:inline-block`}>
+                    <a href="https://api.whatsapp.com/send?phone=593986572316" target="blank" className={`mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:bg-white mt-4 sm:mt-0 hidden sm:inline-block sm:sm:ease-in duration-300`}>
                         <FaWhatsapp className=" text-2xl"/>
                     </a> 
                 </div>
