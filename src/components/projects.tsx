652aa2b60531c949b2268753baa2e6a1d1d74717
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-30 21:58:39 -0500

    project finished

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,8 +1,11 @@
 import imgSimon from '../../public/img/simon.png';
 import imgDicee from '../../public/img/dicee.png';
 import projectc from '../../public/img/projectc++.png';
-import { BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoJquery, BiLogoGithub, BiLogoCPlusPlus } from 'react-icons/bi';
+import detectot from '../../public/img/detector de caras.png';
+import javacrud from '../../public/img/JAVACRTUD.png';
+import { BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoJquery, BiLogoGithub, BiLogoCPlusPlus,BiLogoPython, BiLogoJava } from 'react-icons/bi';
 import { MdOutlinePageview } from 'react-icons/md';
+import { SiMysql } from 'react-icons/si';
 
 
 import Button from './button';
@@ -10,11 +13,10 @@ import Button from './button';
 const Project = () => {
     return (
         <>
-            <div className="flex flex-col p-5 sm:p-16">
                 <h2 className="flex text-5xl font-bold text-yellow-500 justify-center pb-10">Projects</h2>
-                <div className='p-2 sm:p-10 flex flex-col sm:justify-between sm:flex sm:flex-row '>
 
-                    <div className=' flex flex-col p-5 justify-center items-center '>
+                <div className='p-2 lex flex-col sm:p-10 f sm:justify-between sm:flex sm:flex-row '>
+                    <div className=' flex flex-col p-5 justify-center items-center sm:'>
                         <p className="inline text-white font-bold pb-2">SIMON DICEE WEBSITE</p>
                         <img src={imgSimon} className='w-60 h-auto p- rounded-xl'/>
                         <p className='text-white items-start p-7'>Video game about interactive memorization</p>
@@ -69,24 +71,74 @@ const Project = () => {
                         </div>
                             <Button icon={<BiLogoGithub/>} href='https://github.com/edusebass/DICEE-website'>Visit Repo</Button>
                     </div>
-
+                    
                     <div className=' flex flex-col p-5 justify-center items-center'>
                         <p className="inline text-white font-bold pb-2">Control Inventory</p>
                         <img src={projectc} className='w-60 h-auto p-2 rounded-xl'/>
                         <p className='text-white items-start p-7'>Project realized with C++ his function is inventory control of products, and CRUD of admins and clients. The data saved in the files .txt</p>
                         <div className='flex flex-col sm:flex-row'>
-                            <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-1000 p-1 px-3 text-sky-200 text-xl">
+                            <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-1000 p-1 px-3 text-sky-200 text-5xl">
                             <BiLogoCPlusPlus/>
                             <p className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-1000 text-sm text-center text-white">C++</p>        
                         </div>
                         </div>
                         <div className='pt-3'>
                             <Button icon={<BiLogoGithub/>} href='https://github.com/edusebass/import-project-cplusplus.git'>Visit repo</Button>
-                        </div>
-                            
+                        </div>  
                     </div>
+                    
                 </div> 
-            </div>
+                {/*  */}
+                
+                <div className='p-2 lex flex-col sm:p-10 f sm:justify-between sm:flex sm:flex-row'>
+
+                    <div className=' flex flex-col p-5 justify-center items-center'>
+                        <p className="inline text-white font-bold pb-2">App Java Crud</p>
+                        <img src={javacrud} className='w-60 h-auto p-2 rounded-xl'/>
+                        <p className='text-white items-start p-7'>Project realized with C++ his function is inventory control of products, and CRUD of admins and clients. The data saved in the files .txt</p>
+                        <div className='flex flex-col sm:flex-row'>
+                            <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-1000 p-1 px-3 text-white text-6xl">
+                            <BiLogoJava/>
+                            <p className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-1000 text-sm text-center text-white">Java</p>        
+                            </div>
+                            <div className='className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-1000 p-1 px-3 text-white text-6xl'>
+                            <SiMysql/>
+                            <p className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-1000 text-sm text-center text-white">MySQL</p>
+                            </div>
+                        </div>
+                        <div className='pt-3'>
+                            <Button icon={<BiLogoGithub/>} href='https://github.com/edusebass/PruebaPOOSQL.git'>Visit repo</Button>
+                        </div>  
+                    </div>
+
+                    <div className='flex flex-col p-5 justify-center items-center'>
+                        <p className="inline text-white font-bold pb-2">Detector de rostros</p>
+                        <img src={detectot} className='w-60 h-auto p-2 rounded-xl' />
+                        <p className='text-white items-start p-7'>
+                        An app built with Java Swing that handles data connected to a MySQL database.
+                        </p>
+                        <div className='flex flex-col items-center justify-center text-center sm:flex-row'>
+                            <div className="text-blue-300 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-1000 p-1 px-3 text-5xl text-center justify-center">
+                                <div className="flex items-center"> 
+                                    <BiLogoPython className="text-center" />
+                                    <p className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-1000 text-sm text-center text-white ml-1">
+                                        Python
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='pt-3'>
+                            <Button icon={<BiLogoGithub />} href='https://github.com/edusebass/Detector-de-caraas-con-Python.git'>
+                                Visit repo
+                            </Button>
+                        </div>
+                    </div>
+
+
+                    
+                    
+                </div>
+            
         </>
     )
 }
