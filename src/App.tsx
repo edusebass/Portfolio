60121b2d3d06689cc222d81fc4927ab52df37ca3
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-09-01 17:11:59 -0500

    scroll change style

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Footer from './components/footer';
 import Skills from './components/skills';
 import Project from './components/projects';
 import AboutMe from './components/aboutme';
+ 
 
 function App() {
   
   return (
     <>
       <body className="font-mono min-h-screen bg-gradient-to-r from-black to-blue-950">
+        
         <Header />
         <Intro/>
         <Skills/>
--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,15 +1,14 @@
 const AboutMe = () =>{
     return(
         <>
-            <section className="flex flex-col px-14 pb-10">
-                <div className="flex text-5xl font-bold text-yellow-500 justify-center  sm:p-48 sm:text-8xl sm:font-bold">
+            <section className="flex flex-col px-14 pb-10 sm:flex-row sm:px-2">
+                <div className="flex text-5xl font-bold text-yellow-500 justify-center  sm:p-48 sm:text-6xl sm:font-bold">
                     <h2 className="pb-10">About me</h2>
                 </div>
-                <div className="flex sm:px-48 sm:py-52">
+                <div className="flex md:pt-40 md:px-20">
                     <p className="text-white">In 2019, enter coding and web development. Fast forward to today, and I’ve had the privilege of building software for multiple customers as a tire company.<br></br>
                     My main focus these days is building products and leading projects for our customers. In my spare time I also like to create videos related to programming and teaching.<br></br>
                     When I’m not at the computer, I go running and playing football 🙌
-
                     </p>
                 </div>
             </section>
