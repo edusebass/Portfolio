e038e599736db0d086cfa2693e69e914ccf7f0d2
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-09-01 11:19:59 -0500

    aboutme agregado

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import {} from 'react'
 import Header from './components/header';
 import Intro from './components/intro';
 import Footer from './components/footer';
-import AboutMe from './components/aboutme';
+import Skills from './components/skills';
 import Project from './components/projects';
-
+import AboutMe from './components/aboutme';
 
 function App() {
   
@@ -13,13 +13,15 @@ function App() {
       <body className="font-mono min-h-screen bg-gradient-to-r from-black to-blue-950">
         <Header />
         <Intro/>
-        <AboutMe/>
+        <Skills/>
         <Project/>
+        <AboutMe/>
         <Footer/>
+        
         
       </body>
     </>
   )
 }
 
-export default App
+export default App;
