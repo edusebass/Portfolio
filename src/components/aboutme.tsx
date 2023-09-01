5f4f19b61349e8bb6709664d63ce732e745e0338
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-09-01 11:20:31 -0500

    Create aboutme.tsx

---
+++ src/components/aboutme.tsx
@@ -0,0 +1,23 @@
+const AboutMe = () =>{
+    return(
+        <>
+            <section className="flex flex-col px-14 pb-10">
+                <div className="flex text-5xl font-bold text-yellow-500 justify-center  sm:p-48 sm:text-8xl sm:font-bold">
+                    <h2 className="pb-10">About me</h2>
+                </div>
+                <div className="flex sm:px-48 sm:py-52">
+                    <p className="text-white">In 2019, enter coding and web development. Fast forward to today, and I’ve had the privilege of building software for multiple customers as a tire company.<br></br>
+                    My main focus these days is building products and leading projects for our customers. In my spare time I also like to create videos related to programming and teaching.<br></br>
+                    When I’m not at the computer, I go running and playing football 🙌
+
+                    </p>
+                </div>
+            </section>
+            
+            
+        </>    
+    )
+    
+}
+
+export default AboutMe;
