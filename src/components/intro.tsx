6a901cc9c07bbe3819c86d90a1eaf2d4c84c2ad7
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-03 10:17:12 -0500

    buf fixed

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,49 +1,36 @@
-import {useEffect, useState, useRef} from 'react';
+import { useState, useEffect } from 'react';
 import { BiDownload } from 'react-icons/bi';
 import Button from './button';
 import imgPerfil from '../../public/img/fotocarnet.jpg';
 import CV from '../../public/docs/EduardoAlmachiCV.pdf';
-import Typed from 'typed.js';
 
-const intro = () => {
+const Intro = () => {
+  const [text, setText] = useState('');
+  const [index, setIndex] = useState(0);
+  const introText = "Hi, I´m Eduardo Almachi 👋";
 
-    const [shouldAnimate, setShouldAnimate] = useState(false);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setText(introText.substring(0, index));
+      setIndex(index => index + 1);
+    }, 50);
 
-    const textRef = useRef(null);
+    return () => clearInterval(intervalId);
+  }, [index]);
 
-    useEffect(() => {
-        const options = {
-        strings: ["Hi, I´m Eduardo Almachi 👋"],
-        typeSpeed: 50, 
-        showCursor: false, 
-        };
-
-        const typed = new Typed(textRef.current, options);
-
-        return () => {
-        typed.destroy(); 
-        };
-    }, []);
-
-    useEffect(() => {   
-        setShouldAnimate(true)
-    });
-
-    return (
-        <div className='flex px-1 py-5 sm:px-52 sm:py-40 text-white w-full h-full' id='intro'>
-            <div className='flex'>
-                <img src={imgPerfil} className='my-12 hidden sm:block sm:h-auto sm:max-h-56 rounded-full'/>
-                <div className={`px-7 pt-7 sm:px-20 ${ shouldAnimate ? "animate-fade-right animate-once animate-duration-[600ms] animate-delay-300" : ""}`}>
-                    
-                    <span ref={textRef} className=" font-mono whitespace-no-wrap border-r-4 border-solid ">Hi, I´m Eduardo Almachi 👋</span>
-                    
-                    <h2 className='text-5xl font-extrabold text-yellow-400 sm:text-7xl'>Software Developer</h2>
-                    <p className='py-2 text-slate-400 pb-4'>Entusiasta de la tecnología dedicado con una curiosidad insaciable por desentrañar las complejidades de nuestro mundo digital.</p>
-                    <Button icon={<BiDownload/>} href={CV}>Download CV</Button>
-                </div>
-            </div>
+  return (
+    <div className='flex px-1 py-5 sm:px-52 sm:py-40 text-white w-full h-full' id='intro'>
+      <div className='flex'>
+        <img src={imgPerfil} className='my-12 hidden sm:block sm:h-auto sm:max-h-56 rounded-full' />
+        <div className='px-7 pt-7 sm:px-20'>
+          <span className="font-mono whitespace-no-wrap border-r-4 border-solid ">{text}</span>
+          <h2 className='text-5xl font-extrabold text-yellow-400 sm:text-7xl'>Software Developer</h2>
+          <p className='py-2 text-slate-400 pb-4'>Entusiasta de la tecnología dedicado con una curiosidad insaciable por desentrañar las complejidades de nuestro mundo digital.</p>
+          <Button icon={<BiDownload />} href={CV}>Download CV</Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default intro;
+export default Intro;
