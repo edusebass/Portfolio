c6f7cdfd48cf16a624f08c8267a62fa4d932b5a7
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-31 16:38:17 -0500

    update project

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,11 +1,11 @@
 const AboutMe = () =>{
     return(
         <>
-            <div className="flex h-screen flex-col md:pt-20 md:mt-14 md:px-20 " id="aboutme">
-                <h2 className="flex text-center justify-center pb-10 text-6xl font-extrabold text-yellow-500">About me</h2>
+            <div className="flex h-full flex-col mt-8 pt-5 sm:h-screen md:pt-20 md:mt-14 md:px-20 " id="aboutme">
+                <h2 className="flex text-center justify-center pb-10 text-6xl font-extrabold text-yellow-500">Sobre mí</h2>
                 <div className="flex flex-wrap">
                     <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
-                        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                        <div className=" flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                             <div className="px-4 py-5 flex-auto">
                                 <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-black"> 
                                 </div>
@@ -18,7 +18,7 @@ const AboutMe = () =>{
                         </div>
                     </div>
                     <div className="w-full md:w-4/12 px-4 text-center">
-                        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                        <div className=" flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                             <div className="px-4 py-5 flex-auto">
                                 <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-yellow-400">
                                 </div>
@@ -31,7 +31,7 @@ const AboutMe = () =>{
                         </div>
                     </div>
                     <div className="pt-6 w-full md:w-4/12 px-4 text-center">
-                        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                        <div className=" flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                             <div className="px-4 py-5 flex-auto">
                                 <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-indigo-950">    
                                 </div>
@@ -45,6 +45,7 @@ const AboutMe = () =>{
                     </div>
                 </div>       
             </div>
+            
         </>    
     )
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,98 +1,95 @@
 
 
-
-
+import {FaWhatsapp, FaLinkedin, FaGithub} from "react-icons/fa";
 
 const footer = () => {
     return (
-        <>
-            <div className="flex items-center justify-center p-12">
-  
-  <div className="mx-auto w-full max-w-[550px]">
-    <form  method="POST">
-      <div className="mb-5">
-        <label
-          
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Full Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Full Name"
-          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Email Address
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="example@domain.com"
-          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-        
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Subject
-        </label>
-        <input
-          type="text"
-          name="subject"
-          id="subject"
-          placeholder="Enter your subject"
-          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Message
-        </label>
-        <textarea
-         
-          name="message"
-          id="message"
-          placeholder="Type your message"
-          className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        ></textarea>
-      </div>
-      <div>
-        <button
-          className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
-            <footer className="bg-neutral-900 text-center text-white" id="footer">
-                <div className="container px-6 pt-6 mx-auto">
-                    {/* <!-- Social media icons container --> */}
-                    <div className="mb-6 flex justify-center w-full">
-                            <a
+      <>
+        {/* <div className="flex items-center justify-center p-12">
+          <h2 className="flex text-center justify-center pb-10 text-6xl font-extrabold text-yellow-500">Contáctame</h2>
+          <div className="mx-auto w-full max-w-[550px]">
+            <form  method="POST">
+              <div className="mb-5">
+                <label
+                  
+                  className="mb-3 block text-base font-medium text-white"
+                >
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Ingrese su nombre"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  
+                  className="mb-3 block text-base font-medium text-white"
+                >
+                  Email 
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="example@hotmail.com"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                
+                  className="mb-3 block text-base font-medium text-white"
+                >
+                  Asunto
+                </label>
+                <input
+                  type="text"
+                  name="subject"
+                  id="subject"
+                  placeholder="Ingresa el asunto"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  
+                  className="mb-3 block text-base font-medium text-white"
+                >
+                  Mensaje
+                </label>
+                <textarea
+                  name="message"
+                  id="message"
+                  placeholder="Escribe tu mensaje..."
+                  className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                ></textarea>
+              </div>
+              <div>
+                <button
+                  className="hover:shadow-form rounded-md bg-yellow-500 py-3 px-8 text-base font-semibold text-white outline-none"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div> */}
+        <footer className=" bg-neutral-900 text-center text-white" id="footer">
+            <div className=" px-6 pt-6 mx-auto">
+                <div className="mb-6 flex justify-center w-full">
+                <a
                                 href="https://mail.google.com/mail/?view=cm&to=edu03sebas@gmail.com"
                                 target="_blank"
                                 type="button"
-                                className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal hover:bg-white hover:text-black"
+                                className="mx-1 hidden text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:text-red-400 hover:bg-white mt-4 sm:mt-0 sm:inline-block sm:ease-in duration-300"
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
-                                    className="mx-auto h-full w-4"
+                                    className="mx-auto h-full w-6"
                                     fill="currentColor"
                                     viewBox="0 0 24 24">
                                     <path
@@ -101,54 +98,27 @@ const footer = () => {
                                         clipRule="evenodd" />
                                 </svg>
                             </a>
-                            
-                        
-                            <a
-                                href="https://www.linkedin.com/in/eduardo-almachi-727730238/"
-                                target="_blank"
-                                type="button"
-                                className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal transition duration-150 ease-in-out hover:bg-white hover:text-black"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="mx-auto h-full w-4"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24">
-                                    <path
-                                        d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-                                </svg>
-                            </a>
-                        
-                            <a
-                                href="https://github.com/edusebass"
-                                target="_blank"
-                                type="button"
-                                className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal hover:bg-white hover:text-black"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="mx-auto h-full w-4"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24">
-                                    <path
-                                        d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                                </svg>
-                            </a>
-                        
-                    </div>
-                    <div className="mb-6">
-                        <p>
-                        Si te gustó lo que viste, no dudes en contactarme.
-                        </p>
-                    </div>
+                        <a href="https://github.com/edusebass" target="blank" className="mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:text-black hover:bg-white mt-4 sm:mt-0 sm:inline-block sm:ease-in duration-300">
+                            <FaGithub className=" text-2xl"/>                        
+                        </a>
+                        <a href="https://www.linkedin.com/in/eduardo-almachi-727730238/" target="blank" className={`mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent  hover:text-blue-800 hover:bg-white mt-4 sm:mt-0 hidden sm:inline-block sm:sm:ease-in duration-300`}>
+                            <FaLinkedin className=" text-2xl"/>
+                        </a>
+                        <a href="https://api.whatsapp.com/send?phone=593986572316" target="blank" className={`mx-1 text-sm px-2 py-2 leading-none border rounded-full border-neutral-500 hover:border-transparent hover:text-green-600 hover:bg-white mt-4 sm:mt-0 hidden sm:inline-block sm:sm:ease-in duration-300`}>
+                            <FaWhatsapp className=" text-2xl"/>
+                        </a> 
                 </div>
-
-                {/* <!-- Copyright section --> */}
-                <div className="p-4 text-center bg-slate-200 bg-opacity-20">
-                    © 2023 Copyright: Creado con 💓 by Eduardo Almachi
+                <div className="mb-6">
+                  <p>
+                  Si te gustó lo que viste, no dudes en contactarme.
+                  </p>
                 </div>
-            </footer>
-        </>
+            </div>
+            <div className="p-4 text-center bg-slate-200 bg-opacity-20">
+                © 2023 Copyright: Creado con 💓 by Eduardo Almachi
+            </div>
+        </footer>
+      </>
     );
 }
 
