777bbbf4745b5a37285dab9fd06218446bdddb6e
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-07 12:35:45 -0500

    creacion de descripcion y titulo

--- src/components/Experiencia.tsx
+++ src/components/Experiencia.tsx
@@ -1,8 +1,9 @@
 const Experiencia = () => {
     return (
         <>
-            <h2 className="text-yellow-400 font-extrabold text-center pb-8 text-5xl mt-28 " id='projects'>PROYECTOS</h2>
-            <h6 className='text-yellow-400 font-extrabold text-center pb-8 mx-10 sm:mx-0'>Listado de proyectos en los que he trabajado</h6>
+            <h2 className="text-yellow-400 font-extrabold text-center pb-8 text-5xl mt-28 " id='projects'>EXPERIENCIA</h2>
+            <h6 className='text-yellow-400 font-extrabold text-center pb-8 mx-10 sm:mx-0'>Lista de experiencia profesional y descripcion
+            detallada de cada uno de ellos</h6>
         </>
     )
 }
