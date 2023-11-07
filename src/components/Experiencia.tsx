fea37bbfb4e2a8d8626caea88f1e2948fc69b291
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-07 12:34:15 -0500

    creacion experiencia

---
+++ src/components/Experiencia.tsx
@@ -0,0 +1,10 @@
+const Experiencia = () => {
+    return (
+        <>
+            <h2 className="text-yellow-400 font-extrabold text-center pb-8 text-5xl mt-28 " id='projects'>PROYECTOS</h2>
+            <h6 className='text-yellow-400 font-extrabold text-center pb-8 mx-10 sm:mx-0'>Listado de proyectos en los que he trabajado</h6>
+        </>
+    )
+}
+
+export default Experiencia
