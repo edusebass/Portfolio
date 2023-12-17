32ff8aaefbc23953da76cca5b6ffcd64b054a59b
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-16 21:16:47 -0500

    add experiencia niceCode

--- src/components/Experiencia.tsx
+++ src/components/Experiencia.tsx
@@ -1,34 +1,32 @@
+import ExperienciaCard from './ExperienciaCard'
 const Experiencia = () => {
     return (
         <> 
-                <h2 className="text-yellow-400 font-extrabold text-center pb-8 text-5xl mt-28 " id='experiencia'>EXPERIENCIA LABORAL</h2>
-                <h6 className='text-yellow-400 font-extrabold text-center pb-8 mx-10 sm:mx-0'>Lista de experiencia profesional y descripcion
-                detallada de cada uno de ellos</h6>
-                <div className="flex justify-center">
-                <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
-                    <div className=" flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                        <div className="px-4 py-5 flex-auto">
-                            <img src="https://res.cloudinary.com/dwowtb0ya/image/upload/v1699380013/Portfolio/computer-svgrepo-com_qmbbtn.svg"
-                            className="h-20 m-auto" />
-                            <h3 className=" text-3xl font-semibold">Tecniservicios</h3>
-                            <h3 className=" text-xl font-semibold">De Agosto 2022 - Octubre 2022</h3>
-                            <h3 className=" text-lg">Cargo ocupado: Tecnico informático</h3>
-                            <p className="text-left mt-2">
-                                <ol>
-                                    <li>- Mantenimiento y correctivo de computadoras e impresoras.</li>
-                                    <li>- Optimizacion de aplicativos web.</li>
-                                    <li>- Asesoria de sistemas informaticos.</li>
-                                    <li>- Creacion de landing page para ventas.</li>
-                                </ol>
-                            </p>
-                            <h3 className="text-lg mt-2">Proyecto</h3>
-                            <p className="text-left">
-                                Landing page: <a href="llantas.vercel.app">llantas.vercel.app</a>
-                            </p>
-                            
-                        </div>
-                    </div>
-                </div>
+            <h2 className="text-yellow-400 font-extrabold text-center pb-8 text-5xl mt-28 " id='experiencia'>EXPERIENCIA LABORAL</h2>
+            <h6 className='text-yellow-400 font-extrabold text-center pb-8 mx-10 sm:mx-0'>Lista de experiencia profesional y descripcion
+            detallada de cada uno de ellos</h6>
+            <div className="flex flex-col sm:flex-row sm:flex-wrap justify-center">
+                
+                    <ExperienciaCard 
+                        cargo='Tecnico informático'
+                        fecha='De Agosto 2022 - Octubre 2022'
+                        lugar='Tecniservicios'
+                        opcion1='Mantenimiento y correctivo de computadoras e impresoras.'
+                        opcion2='Optimizacion de aplicativos web.'
+                        opcion3='Asesoria de sistemas informaticos.'
+                        opcion4='Creacion de landing page para ventas.'
+                        linkProyecto='https://llantas.vercel.app/'
+                    />
+                    <ExperienciaCard 
+                        cargo='Desarrollador FullStack'
+                        fecha='De Noviembre 2023 - Actualidad'
+                        lugar='NiceCode'
+                        opcion1='Demostré habilidades sólidas de resolución de problemas.'
+                        opcion2='Identificación y reescritura de algoritmos para mejorar la eficiencia temporal y reducir la complejidad.'
+                        opcion3='Implementación de scripts y herramientas personalizadas para automatizar procesos repetitivos.'
+                        opcion4='Creacion de componentes en React y manejo de backend.'
+                        proyectoVisibles='hidden'
+                    />
             </div>
         </>
     )
--- src/components/ExperienciaCard.tsx
+++ src/components/ExperienciaCard.tsx
@@ -1,26 +1,47 @@
-const ExperienciaCard = () => { 
+import AnimatedDiv from './AnimatedDiv'
+
+interface Props {
+    lugar: string,
+    fecha: string,
+    cargo: string,
+    opcion1?: string,
+    opcion2?: string,
+    opcion3?: string,
+    opcion4?: string,
+    linkProyecto?: string,
+    proyectoVisibles?: string
+}
+
+const ExperienciaCard = ({lugar, fecha, cargo, opcion1, opcion2, opcion3, opcion4, linkProyecto, proyectoVisibles}: Props) => { 
     return (
         <>
-            <div className="px-4 py-5 flex-auto">
-                <img src="https://res.cloudinary.com/dwowtb0ya/image/upload/v1699380013/Portfolio/computer-svgrepo-com_qmbbtn.svg"
-                className="h-20 m-auto" />
-                <h3 className=" text-3xl font-semibold">Tecniservicios</h3>
-                <h3 className=" text-xl font-semibold">De Agosto 20223- Octubre 2022</h3>
-                <h3 className=" text-lg">Cargo ocupado: Tecnico informático</h3>
-                <p className="text-left mt-2">
-                    <ol>
-                        <li>- Mantenimiento y correctivo de computadoras e impresoras.</li>
-                        <li>- Optimizacion de aplicativos web.</li>
-                        <li>- Asesoria de sistemas informaticos.</li>
-                        <li>- Creacion de landing page para ventas.</li>
-                    </ol>
-                </p>
-                <h3 className="text-lg mt-2">Proyecto</h3>
-                <p className="text-left">
-                    Landing page: <a href="llantas.vercel.app">llantas.vercel.app</a>
-                </p>
+        <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center ">
+            
+            <AnimatedDiv x={0.5} y={50}>
                 
+            <div className=" flex min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg ">    
+                <div className="px-4 py-5 flex-auto">
+                    <img src="https://res.cloudinary.com/dwowtb0ya/image/upload/v1699380013/Portfolio/computer-svgrepo-com_qmbbtn.svg"
+                    className="h-20 m-auto" />
+                    <h3 className=" text-3xl font-semibold">{lugar}</h3>
+                    <h3 className=" text-xl font-semibold">{fecha}</h3>
+                    <h3 className=" text-lg">Cargo ocupado: {cargo}</h3>
+                    <p className="text-left mt-2">
+                        <ol>
+                            <li>- {opcion1}</li>
+                            <li>- {opcion2}</li>
+                            <li>- {opcion3}</li>
+                            <li>- {opcion4}</li>
+                        </ol>
+                    </p>
+                    <h3 className={`text-lg mt-2 ${proyectoVisibles}`}>Proyecto</h3>
+                    <p className="text-left">
+                        <a href={linkProyecto}>{linkProyecto}</a>
+                    </p>
+                </div>
             </div>
+            </AnimatedDiv>
+        </div>
             
         </>
     )
