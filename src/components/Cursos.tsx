3ac13f1c722b0558e8ba5d7bb2261b5ac7bb1db6
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-17 22:19:13 -0500

    props de link

--- src/components/Cursos.tsx
+++ src/components/Cursos.tsx
@@ -10,35 +10,36 @@ const Cursos = () => {
                     stylesWords="hover:text-white"
                     fromTop
                 />
-                <img src="https://upload.wikimedia.org/wikipedia/commons/3/32/Platzi.jpg" className="w-36 rounded-xl mt-4"/>
-                <div className=" grid items-center max-w-6xl grid-cols-1 sm:grid-cols-2 gap-4 mx-auto mt-6 md:grid-cols-4  ">
+                <img src="https://logowik.com/content/uploads/images/udemy-new-20212512.jpg" className="w-36 h-auto rounded-xl mt-4"/>                <div className=" grid items-center max-w-6xl grid-cols-1 sm:grid-cols-2 gap-4 mx-auto mt-6 md:grid-cols-4  ">
                     <CursosCard 
                         imgCurso=""
                         tituloCursos="Python sin fronteras: HTML, CSS, Flask y MySQL"
+                        linkCurso="https://1drv.ms/b/s!AiGeBBzH0ZjfouxaBLd2ZM16i-uU5A?e=wbpPvC"
                     />
                     <CursosCard 
                         imgCurso=""
                         tituloCursos="Master en JavaScript: Aprender JS, jQuery, Angular, NodeJS"
+                        linkCurso="https://1drv.ms/b/s!AiGeBBzH0Zjfouxb3_ej1MjyWGRzQQ?e=AFHSev"
                     />
                     <CursosCard 
                         imgCurso=""
                         tituloCursos="The Complete 2023 Web Development Bootcamp"
+                        linkCurso=""
                     />
-                    <CursosCard 
-                        imgCurso=""
-                        tituloCursos="Curso de React.js con TypeScript"
-                    />
+                    
                 </div>
                 <div>
                     <br />
                     <br />
                 </div>
 
-                <img src="https://logowik.com/content/uploads/images/udemy-new-20212512.jpg" className="w-36 h-auto rounded-xl mt-4"/>
+                
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/32/Platzi.jpg" className="w-36 rounded-xl mt-4"/>
                 <div className=" grid items-center max-w-6xl grid-cols-2 gap-4 mx-auto mt-6 md:grid-cols-4">
                     <CursosCard 
                         imgCurso=""
                         tituloCursos="Curso de React.js con TypeScript"
+                        linkCurso="https://1drv.ms/b/s!AiGeBBzH0Zjfoutdf4eT7Rk01k8jTA?e=zf1xy0"
                     />
                     
                 </div>
--- src/components/CursosCard.tsx
+++ src/components/CursosCard.tsx
@@ -1,19 +1,25 @@
-// import { CiShare1 } from "react-icons/ci";
+import { CiShare1 } from "react-icons/ci";
 
 interface Props {
     imgCurso: string;
     tituloCursos: string;
+    linkCurso: string;
 }
 
-const CursosCard = ({tituloCursos}: Props) => {  
+const CursosCard = ({tituloCursos, linkCurso}: Props) => {  
     return (
         <>
-            <div className="bg-slate-200 h-24 w-60 p-4 flex shadow-lg items-center justify-center rounded-2xl">
-                <h1>{tituloCursos}</h1>
-                
-                {/* <CiShare1  /> */}
-                
-            </div>
+            <a href={linkCurso} target="_blank">
+            {/* <a href="https://1drv.ms/b/s!AiGeBBzH0Zjfoutdf4eT7Rk01k8jTA?e=IyjLk1" target="_blank"> */}
+                <div className="bg-slate-200 h-24 w-60 p-4 flex shadow-lg items-center justify-center rounded-2xl hover:bg-white">
+                    <div>
+                        <h1>{tituloCursos}</h1>
+                    </div>
+                    <div className="">
+                        <CiShare1  className="text-3xl"/>
+                    </div>
+                </div>
+            </a>
         </>
     )
 }
